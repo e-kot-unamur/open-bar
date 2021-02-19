bb52f0ab50b097805c6b373022f615423573341f
tsepton <tsepton@gmail.com>
2021-02-19 11:33:04 +0100

    Add watermark

--- client/src/views/settings/Settings.svelte
+++ client/src/views/settings/Settings.svelte
@@ -3,10 +3,12 @@
   import Reset from "./Reset.svelte";
   import PriceSection from "./PriceSection.svelte";
   import Container from "../../components/Container.svelte";
+  import Watermark from "./Watermark.svelte";
 </script>
 
 <Container>
   <PriceSection />
   <DebtRecap />
   <Reset />
+  <Watermark />
 </Container>
---
+++ client/src/views/settings/Watermark.svelte
@@ -0,0 +1,15 @@
+<div>
+    Thibaut Septon, MIT Licensed
+</div>
+
+<style>
+    div {
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        font-weight: 200;
+        font-size: small;
+        color: rgba(0, 0, 0, 0.3);
+        padding: 10px;
+    }
+</style>
