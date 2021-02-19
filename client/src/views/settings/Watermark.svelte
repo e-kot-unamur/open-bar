d940e404865eb29d8ec1f3fb40bea8f87475f665
tsepton <tsepton@gmail.com>
2021-02-19 12:08:00 +0100

    Update watermark

--- client/src/views/settings/Watermark.svelte
+++ client/src/views/settings/Watermark.svelte
@@ -1,15 +1,15 @@
-<div>
-    Thibaut Septon, MIT Licensed
-</div>
+<span>Thibaut Septon, MIT Licensed</span>
 
 <style>
-    div {
-        position: fixed;
-        bottom: 0;
-        right: 0;
-        font-weight: 200;
-        font-size: small;
-        color: rgba(0, 0, 0, 0.3);
-        padding: 10px;
-    }
+  span {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    font-weight: 200;
+    font-size: small;
+    padding: 10px;
+    color: rgba(100, 100, 100, 0.65);
+    z-index: 100 !important;
+  }
+
 </style>
